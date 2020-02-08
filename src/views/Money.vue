497fc78b69416f371f022f7cd0f39a3c77e4bffb
Ories Zhu <tellt@126.com>
2020-02-08 18:16:25 +0800

    封装出一个 model.ts

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,12 +14,12 @@
   import Notes from "@/components/Money/Notes.vue"
   import Tags from "@/components/Money/Tags.vue"
   import {Component, Watch} from "vue-property-decorator"
+  import model from '@/model'
 
-  const recordList: Record[] = JSON.parse(window.localStorage.getItem("recordList") || "[]")
+  const recordList = model.fetch()
 
 
-
-  type Record = {
+  type RecordItem = {
     tags: string[]
     notes: string
     type: string
@@ -32,8 +32,8 @@
   })
   export default class Money extends Vue {
     tags = ["衣", "食", "住", "行"]
-    recordList: Record[] = recordList
-    record: Record = {
+    recordList: RecordItem[] = recordList
+    record: RecordItem = {
       tags: [], notes: "", type: "-", amount: 0
     }
 
@@ -46,7 +46,7 @@
     }
 
     saveRecord() {
-      const record2: Record = JSON.parse(JSON.stringify(this.record))
+      const record2: RecordItem = model.clone(this.record)
       record2.createdAt = new Date()
       this.recordList.push(record2)
       console.log(this.recordList)
@@ -54,7 +54,7 @@
 
     @Watch("recordList")
     onRecordListChange() {
-      window.localStorage.setItem("recordList", JSON.stringify(this.recordList))
+      model.save(this.recordList)
     }
   }
 </script>
