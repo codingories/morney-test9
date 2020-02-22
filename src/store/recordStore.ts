3c1d4448300ff30582ccd6e465722541340fc65b
Ories Zhu <tellt@126.com>
2020-02-22 11:19:32 +0800

    模块化 store ， 删除了 models

---
+++ src/store/recordStore.ts
@@ -0,0 +1,30 @@
+import clone from '@/lib/clone'
+
+const localStorageKeyName = 'recordList'
+
+// let data: RecordItem[] | undefined = undefined;
+
+
+
+const recordStore = {
+  recordList: [] as RecordItem[],
+  // record store
+  fetchRecords() {
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+  },
+  saveRecords(){
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+  },
+  // fetch 访问不到，因为fetch不是变量了，现在的fetch是一个key,this.fetch的this是根据函数确定的
+  // 这里没有函数，所以是全局的this,也就是window,给它一个recordStore的名字即可
+  createRecord(record: RecordItem) {
+    const record2: RecordItem = clone(record)
+    record2.createdAt = new Date()
+    this.recordList && this.recordList.push(record2)
+    recordStore.saveRecords()
+  },
+}
+recordStore.fetchRecords()
+
+export default recordStore;
