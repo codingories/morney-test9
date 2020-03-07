1990f4dd68ed0d6e7526d16fa697bb893d3c03cf
Ories Zhu <tellt@126.com>
2020-03-07 09:17:58 +0800

    修复一进页面就造成commit引发的bug

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -24,6 +24,7 @@ type TagListModel = {
 type RootState = {
   recordList: RecordItem[],
   createRecordError: Error | null,
+  createTagError: Error | null,
   tagList: Tag[],
   currentTag?: Tag
 }
